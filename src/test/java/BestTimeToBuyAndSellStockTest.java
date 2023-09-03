0366e44e0fe4f0b085f448bcaaaafdd9fc31ec80
mohitkalia97 <kalia.mohit1997@gmail.com>
2023-09-03 14:10:31 +0200

    ContainsDuplicate

---
+++ src/test/java/BestTimeToBuyAndSellStockTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class BestTimeToBuyAndSellStockTest {
+    @ParameterizedTest
+    @MethodSource("testCases")
+    public void testSolution(int[] prices, int expected){
+
+        int result = BestTimeToBuyAndSellStock.maxProfit(prices);
+
+        Assertions.assertEquals(expected, result);
+    }
+
+    static Stream<Arguments> testCases(){
+        return Stream.of(
+                Arguments.of(new int[]{7,1,5,3,6,4}, 5),
+                Arguments.of(new int[]{7,6,4,3,1}, 0),
+                Arguments.of(new int[]{7,6,1,4,3}, 1)
+
+        );
+    }
+}
