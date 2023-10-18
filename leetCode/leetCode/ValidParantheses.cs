72726a0dcdb3f186683f42174efa42b4bfcb29ad
Mohit Kalia <m.kalia@smc.at>
2023-10-18 09:57:50 +0200

    fixed project structure

--- leetCode/leetCode/ValidParantheses.cs
+++ leetCode/leetCode/ValidParantheses.cs
@@ -4,9 +4,63 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+Example 3:
+
+Input: s = "(]"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+ */
+
 namespace leetCode
 {
-    internal class ValidParantheses
+    public  class ValidParantheses
     {
+            public static bool IsValid(string s)
+            {
+            Stack<Char> stack = new Stack<Char>();
+
+                if (s[0] == '[' || s[0] == '(')
+                { 
+                    stack.Push(s[0]);
+                    if (s[1] == ']')
+                    {
+                        if(s[1] == stack.Pop())
+                        {
+                            return true;
+                        }
+                    }   
+                 
+                }
+                
+                if (s[0] == ')' || s[0] == ']')
+                {
+                    return false;
+                }
+
+                return false;
+            }
     }
 }
---
+++ leetCode/test/ValidParanthesesTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leetCode;
+
+namespace test
+{
+    [TestClass]
+    public class ValidParanthesesTest
+    {
+        [DataTestMethod]
+        [DataRow ("()",true)]
+        [DataRow ("()[]{}", true)]
+        [DataRow ("(]", false)]
+
+        public void IsValidTest(string s, bool expected)
+        {
+            bool result = ValidParantheses.IsValid(s);
+
+            Assert.Equals(result, expected);
+        }
+    }
+
+}
