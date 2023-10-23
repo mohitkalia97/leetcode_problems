e15c1bc4c6aa9ca4b39ac321ea1b7fa261154419
Mohit Kalia <m.kalia@smc.at>
2023-10-23 08:27:46 +0200

    created test for ValidPalindrome

---
+++ leetCode/test/ValidPalindromeTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leetCode;
+
+namespace test
+{
+    [TestClass]
+    public class ValidPalindromeTest
+    {
+        [DataTestMethod]
+        [DataRow("A man, a plan, a canal: Panama", true)]
+        [DataRow("race a car", false)]
+        [DataRow(" ", true)]
+        public void IsPalindromeTest(string s, bool expected) 
+        {
+            bool res = ValidPalindrome.IsPalindrome(s);
+
+            Assert.AreEqual(res, expected); 
+        }
+    }
+}
