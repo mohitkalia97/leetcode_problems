68950478405dde3dfeba7daed43466f5550e9b02
Mohit Kalia <m.kalia@smc.at>
2023-10-23 16:40:22 +0200

    ValidAnagram test done

---
+++ leetCode/leetCode/ValidAnagram.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode
+{
+
+    public class ValidAnagram 
+    {
+        public static bool IsAnagram(string s, string t)
+        {
+
+        }
+    }
+}
---
+++ leetCode/test/ValidAnagramTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leetCode;
+
+namespace test
+{
+    [TestClass]
+    public class ValidAnagramTest
+    {
+        [DynamicData(nameof(GetTestData),DynamicDataSourceType.Method )]
+        public void isAnagramTest(string s, string t, bool ex)
+        {
+
+            bool res = ValidAnagram.IsAnagram(s,t);
+
+            Assert.AreEqual(res, ex);
+        }
+
+        public static IEnumerable<object> GetTestData()
+        {
+            yield return new object[]
+            {
+                "anagram", "nagaram", true
+            };
+            yield return new object[]
+            {
+                "rat", "car", false
+            };
+        }
+    }
+}
