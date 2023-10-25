669d5fe9f1405515f24aa4dfc9d2489939a27c8e
Mohit Kalia <m.kalia@smc.at>
2023-10-25 10:02:00 +0200

    BinarySearch Test done

---
+++ leetCode/leetCode/BinarySearch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetCode
+{
+    public class BinarySearch
+    {
+        public static int Search(int[] nums, int target)
+        {
+            return 4;
+        }
+    }
+}
---
+++ leetCode/test/BinarySearchTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leetCode;
+
+namespace test
+{
+    [TestClass]
+    public class BinarySearchTest
+    {
+        [DataTestMethod]
+        [DynamicData(nameof(GetTestData), DynamicDataSourceType.Method)]
+        public void testSearch(int[] nums, int target, int ex)
+        {
+            int res = BinarySearch.Search(nums, target);
+            Assert.AreEqual(ex, res);   
+        }
+
+        public static IEnumerable<object[]> GetTestData()
+        {
+            yield return new object[] {
+                new int[] {-1,0,3,5,9,12}, 9, 4
+            };
+
+            yield return new object[] {
+                new int[]{-1,0,3,5,9,12}, 2, -1
+            };
+        }
+    }
+}
