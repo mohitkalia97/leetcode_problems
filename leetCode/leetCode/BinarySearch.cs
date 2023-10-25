5af3293effb60300f92341411c3aa96c69fac20f
Mohit Kalia <m.kalia@smc.at>
2023-10-25 14:21:15 +0200

    BinarySearch Solution done

--- leetCode/leetCode/BinarySearch.cs
+++ leetCode/leetCode/BinarySearch.cs
@@ -8,9 +8,32 @@ namespace leetCode
 {
     public class BinarySearch
     {
+        //[-1,0,3,5,9,12] //target = 9
         public static int Search(int[] nums, int target)
         {
-            return 4;
+            int start = 0;                        
+            int end = nums.Length - 1; 
+
+            while (start <= end)
+            {
+                int mid = (start + end) / 2; 
+
+                if (nums[mid] == target) 
+                {
+                    return mid;
+                }
+
+                else if (nums[mid] > target) 
+                {
+                    start = mid + 1; 
+                }
+                else
+                {
+                    end = mid - 1; 
+                }
+            }
+            return -1;
+
         }
     }
 }
