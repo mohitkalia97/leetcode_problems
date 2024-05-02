978aafa184fa5e128bf2779974f695a7e303a59f
mohitkalia97 <kalia.mohit1997@gmail.com>
2024-05-03 01:26:35 +0200

    RomanToIntegerTest Done

---
+++ leetCode/test/RomanToIntegerTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using leetCode;
+
+
+namespace test
+{
+    [TestClass]
+    public class RomanToIntegerTest
+    {
+        [DataTestMethod]
+        [DataRow("III"   , 3)]
+        [DataRow("LVIII" , 58)]
+        [DataRow("MCMXCIV", 1994)]
+        [DataRow("MCMXCVII", 1997)] // mybirthyear hehe
+
+        public void RomanToIntTest(string s, int expected) {
+            int result = RomanToInteger.RomanToInt(s);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
