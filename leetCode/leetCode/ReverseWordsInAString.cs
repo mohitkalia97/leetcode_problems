7f828cb6681adc9b2bd83437ee5d67afff1b9d70
mohitkalia97 <kalia.mohit1997@gmail.com>
2024-05-05 20:03:51 +0200

    ReverseWordsInAString Done

---
+++ leetCode/leetCode/ReverseWordsInAString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+//Example 1:
+
+//Input: s = "the sky is blue"
+//Output: "blue is sky the"
+//Example 2:
+
+//Input: s = "  hello world  "
+//Output: "world hello"
+//Explanation: Your reversed string should not contain leading or trailing spaces.
+//Example 3:
+
+//Input: s = "a good   example"
+//Output: "example good a"
+//Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+
+namespace leetCode
+{
+    public class ReverseWordsInAString
+    {
+            public string ReverseWords(string s)
+            {
+                string[] wordsArray = s.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                Array.Reverse(wordsArray);
+                string result = String.Join(" ", wordsArray);
+                return result;
+            }
+    }
+}
